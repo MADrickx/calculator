b3188d3abd6de95e60582bbbd38912af0adf43bd
Audric Skivée <audricskivee@gmail.com>
2022-04-22 12:14:33 +0200

    apiPath

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -3,8 +3,8 @@ import {environment} from '../../environments/environment';
 
 export class GlobalConstants {
   // public static apiPath = environment.apiUrl;
-  public static apiPath = 'https://localhost/api/';
-  // public static apiPath = 'https://cabai.cloud/api/';
+  // public static apiPath = 'https://localhost/api/';
+  public static apiPath = 'https://api-2boost.mydevel.com';
 
   public static httpOptions(token: string | null = '', type: string | null = null): any {
     return {
